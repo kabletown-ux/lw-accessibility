15da288776f50b2201544735b7956a86f431f162
haltersweb <adina_halter@cable.comcast.com>
2014-11-07 15:00:51 -0500

    finalized HTML and bound open/close overlay to all triggers

--- js/lw-accessibility.js
+++ js/lw-accessibility.js
@@ -6,6 +6,20 @@ $(document).ready(function () {
     var $overlay = $('#overlay'),
         $closeButton = $('.close-btn');
     $('[aria-controls="overlay"]').on('click', function (evt) {
+        var idOfOverlayContent = $(this).attr('href').split("#").pop();
+        console.log(idOfOverlayContent);
+        //close current overlay if necesssary
+        if ($overlay.hasClass('reveal')) {
+            //hide the overlay
+            NAMESP.overlay.hideOverlay($overlay);
+            //hide all nestd overlay content
+            $overlay.find('.overlay-content').removeClass('reveal');
+        }
+        //may need to put a delay here for animation purposes
+
+        //show correct content in the DOM
+            $overlay.find('#' + idOfOverlayContent).addClass('reveal');
+        //show overlay
         NAMESP.overlay.showOverlay($overlay, $closeButton);
     });
     $closeButton.on('click', function (evt) {
